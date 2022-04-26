13ca473fcff63a13bcd1423311bf09261a1f6643
ivo <ivailo2250@abv.bg>
2022-04-26 17:25:32 +0300

    fix superuser can make other user group member

--- game_hub/game_hub/accounts/admin.py
+++ game_hub/game_hub/accounts/admin.py
@@ -6,6 +6,64 @@ from django.contrib.auth import get_user_model
 from game_hub.accounts.models import GameHubUserManager, Profile
 from game_hub.games.models import Game
 
+
+from django import forms
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.admin.widgets import FilteredSelectMultiple    
+from django.contrib.auth.models import Group
+
+
+User = get_user_model()
+
+
+# Create ModelForm based on the Group model.
+class GroupAdminForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        exclude = []
+
+    # Add the users field.
+    users = forms.ModelMultipleChoiceField(
+         queryset=User.objects.all(), 
+         required=False,
+         # Use the pretty 'filter_horizontal widget'.
+         widget=FilteredSelectMultiple('users', False)
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Do the normal form initialisation.
+        super(GroupAdminForm, self).__init__(*args, **kwargs)
+        # If it is an existing group (saved objects have a pk).
+        if self.instance.pk:
+            # Populate the users field with the current Group users.
+            self.fields['users'].initial = self.instance.user_set.all()
+
+    def save_m2m(self):
+        # Add the users to the Group.
+        self.instance.user_set.set(self.cleaned_data['users'])
+
+    def save(self, *args, **kwargs):
+        # Default save
+        instance = super(GroupAdminForm, self).save()
+        # Save many-to-many data
+        self.save_m2m()
+        return instance
+    
+    
+# Unregister the original Group admin.
+admin.site.unregister(Group)
+
+# Create a new Group admin.
+class GroupAdmin(admin.ModelAdmin):
+    # Use our custom form.
+    form = GroupAdminForm
+    # Filter permissions horizontal as well.
+    filter_horizontal = ['permissions']
+
+# Register the new Group ModelAdmin.
+admin.site.register(Group, GroupAdmin)
+
 GameUser = get_user_model()
 
 
@@ -25,4 +83,9 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'gender', )
+
+
+
+
+
